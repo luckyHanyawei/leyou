70b79895547664c283241f188cc5088fe07f9a3a
Administrator <1219973395@qq.com>
2021-03-09 16:40:30 +0800

    添加1111111222222222222

--- ly-user/ly-user-service/src/main/java/com/leyou/user/controller/GitTestController.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/controller/GitTestController.java
@@ -1,4 +1,8 @@
 package com.leyou.user.controller;
 
 public class GitTestController {
+
+    public static void main(String[] args) {
+        System.out.println("11111111111111111111111111111");
+    }
 }
--- ly-user/ly-user-service/src/main/java/com/leyou/user/controller/TestController.java
+++ ly-user/ly-user-service/src/main/java/com/leyou/user/controller/TestController.java
@@ -4,5 +4,6 @@ public class TestController {
 
     public static void main(String[] args) {
         System.out.println("I am hyw");
+        System.out.println("2222222222222222222");
     }
 }
